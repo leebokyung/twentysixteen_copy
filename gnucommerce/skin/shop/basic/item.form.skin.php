f64109f0fa403b58002b8af29f5ad523234b3b4f
leebokyung <ipuiz202@naver.com>
2016-05-20 17:19:43 +0900

    그누커머스 쇼핑몰 스킨 수정

--- gnucommerce/skin/shop/basic/item.form.skin.php
+++ gnucommerce/skin/shop/basic/item.form.skin.php
@@ -55,147 +55,150 @@ $gc_ajax_nonce = wp_create_nonce( "item_form" );
                 }
                 ?>
             </div>
-            <div id="sit_star_sns" >
-                <?php if ($star_score) { ?>
-                고객평점 <span>별<?php echo $star_score?>개</span>
-                <img src="<?php echo GC_DIR_URL; ?>img/s_star<?php echo $star_score?>.png" alt="" class="sit_star">
-                <?php } ?>
-                <?php echo $sns_share_links; ?>
-            </div>
-           
         </li>
         <!-- } 상품이미지 미리보기 끝 -->
 
         <!-- 상품 요약정보 및 구매 시작 { -->
         <li id="sit_ov" class="w50">
-            <h2 id="sit_title"><?php echo stripslashes($post->post_title); ?> <span class="sound_only"><?php _e('요약정보 및 구매', GC_NAME); ?></span></h2>
-            <p id="sit_desc"><?php echo $it['it_basic']; ?></p>
-            <?php if($is_orderable) { ?>
-            <p id="sit_opt_info">
-                <?php echo sprintf(__('상품 선택옵션 %s 개, 추가옵션 %s 개', GC_NAME), $option_count, $supply_count); ?>
-            </p>
-            <?php } ?>
+            <h2 id="sit_title">
+                <?php echo stripslashes($post->post_title); ?> <span class="sound_only"><?php _e('요약정보 및 구매', GC_NAME); ?></span>  
+            </h2>
+            <div id="sit_info">
+                <p id="sit_desc"><?php echo $it['it_basic']; ?></p>
+                            
+                <?php if($is_orderable) { ?>
+                <p id="sit_opt_info">
+                    <?php echo sprintf(__('상품 선택옵션 %s 개, 추가옵션 %s 개', GC_NAME), $option_count, $supply_count); ?>
+                </p>
+                <?php } ?>
             
-            <ul class="sit_ov_box">
-            <?php if ($it['it_maker']) { ?>
-            <li>
-                <span class="ov_row"><?php _e('제조사', GC_NAME); ?></span>
-                <span class="ov_description"><?php echo $it['it_maker']; ?></span>
-            </li>
-            <?php } ?>
-
-            <?php if ($it['it_origin']) { ?>
-            <li>
-                <span class="ov_row"><?php _e('원산지', GC_NAME); ?></span>
-                <span class="ov_description"><?php echo $it['it_origin']; ?></span>
-            </li>
-            <?php } ?>
-
-            <?php if ($it['it_brand']) { ?>
-            <li>
-                <span class="ov_row"><?php _e('브랜드', GC_NAME); ?></span>
-                <span class="ov_description"><?php echo $it['it_brand']; ?></span>
-            </li>
-            <?php } ?>
-
-            <?php if ($it['it_model']) { ?>
-            <li>
-                <span class="ov_row"><?php _e('모델', GC_NAME); ?></span>
-                <span class="ov_description"><?php echo $it['it_model']; ?></span>
-            </li>
-            <?php } ?>
-
-            <?php if (!$it['it_use']) { // 판매가능이 아닐 경우 ?>
-            <li>
-                <span class="ov_row"><?php _e('판매가격', GC_NAME); ?></span>
-                <span class="ov_description"><?php _e('판매중지', GC_NAME); ?></span>
-            </li>
-            <?php } else if ($it['it_tel_inq']) { // 전화문의일 경우 ?>
-            <li>
-                <span class="ov_row"><?php _e('판매가격', GC_NAME); ?></span>
-                <span class="ov_description"><?php _e('전화문의', GC_NAME); ?></span>
-            </li>
-            <?php } else { // 전화문의가 아닐 경우?>
-            <?php if ($it['it_cust_price']) { ?>
-            <li>
-                <span class="ov_row"><?php _e('시중가격', GC_NAME); ?></span>
-                <span class="ov_description"><?php echo gc_display_price($it['it_cust_price']); ?></span>
-            </li>
-            <?php } // 시중가격 끝 ?>
-
-            <li>
-                <span class="ov_row"><?php _e('판매가격', GC_NAME); ?></span>
-                <span class="ov_description">
-                    <?php echo gc_display_price(gc_get_price($it)); ?>
-                    <input type="hidden" id="it_price" value="<?php echo gc_get_price($it); ?>">
-                </span>
-            </li>
-            <?php } ?>
-
-            <li>
-                <span class="ov_row"><?php _e('재고수량', GC_NAME); ?></span>
-                <span class="ov_description">
-                <?php echo sprintf(__('%s 개', GC_NAME), gc_number_format(gc_get_it_stock_qty($it['it_id'])) ); ?>
-                </span>
-            </li>
-
-            <?php if ($config['cf_use_mileage']) { // 적립금을 사용한다면 ?>
-            <li>
-                <span class="ov_row"><?php _e('적립금', GC_NAME); ?></span>
-                <span class="ov_description">
+                <ul class="sit_ov_box">
+                    <?php if ($it['it_maker']) { ?>
+                    <li>
+                        <span class="ov_row"><?php _e('제조사', GC_NAME); ?></span>
+                        <span class="ov_description"><?php echo $it['it_maker']; ?></span>
+                    </li>
+                    <?php } ?>
+        
+                    <?php if ($it['it_origin']) { ?>
+                    <li>
+                        <span class="ov_row"><?php _e('원산지', GC_NAME); ?></span>
+                        <span class="ov_description"><?php echo $it['it_origin']; ?></span>
+                    </li>
+                    <?php } ?>
+        
+                    <?php if ($it['it_brand']) { ?>
+                    <li>
+                        <span class="ov_row"><?php _e('브랜드', GC_NAME); ?></span>
+                        <span class="ov_description"><?php echo $it['it_brand']; ?></span>
+                    </li>
+                    <?php } ?>
+        
+                    <?php if ($it['it_model']) { ?>
+                    <li>
+                        <span class="ov_row"><?php _e('모델', GC_NAME); ?></span>
+                        <span class="ov_description"><?php echo $it['it_model']; ?></span>
+                    </li>
+                    <?php } ?>
+        
+                    <?php if (!$it['it_use']) { // 판매가능이 아닐 경우 ?>
+                    <li>
+                        <span class="ov_row"><?php _e('판매가격', GC_NAME); ?></span>
+                        <span class="ov_description"><?php _e('판매중지', GC_NAME); ?></span>
+                    </li>
+                    <?php } else if ($it['it_tel_inq']) { // 전화문의일 경우 ?>
+                    <li>
+                        <span class="ov_row"><?php _e('판매가격', GC_NAME); ?></span>
+                        <span class="ov_description"><?php _e('전화문의', GC_NAME); ?></span>
+                    </li>
+                    <?php } else { // 전화문의가 아닐 경우?>
+                    <?php if ($it['it_cust_price']) { ?>
+                    <li>
+                        <span class="ov_row"><?php _e('시중가격', GC_NAME); ?></span>
+                        <span class="ov_description"><?php echo gc_display_price($it['it_cust_price']); ?></span>
+                    </li>
+                    <?php } // 시중가격 끝 ?>
+        
+                    <li>
+                        <span class="ov_row"><?php _e('판매가격', GC_NAME); ?></span>
+                        <span class="ov_description">
+                            <?php echo gc_display_price(gc_get_price($it)); ?>
+                            <input type="hidden" id="it_price" value="<?php echo gc_get_price($it); ?>">
+                        </span>
+                    </li>
+                    <?php } ?>
+        
+                    <li>
+                        <span class="ov_row"><?php _e('재고수량', GC_NAME); ?></span>
+                        <span class="ov_description">
+                        <?php echo sprintf(__('%s 개', GC_NAME), gc_number_format(gc_get_it_stock_qty($it['it_id'])) ); ?>
+                        </span>
+                    </li>
+        
+                    <?php if ($config['cf_use_mileage']) { // 적립금을 사용한다면 ?>
+                    <li>
+                        <span class="ov_row"><?php _e('적립금', GC_NAME); ?></span>
+                        <span class="ov_description">
+                            <?php
+                            if($it['it_point_type'] == 2) {
+                                echo '구매금액(추가옵션 제외)의 '.$it['it_point'].'%';
+                            } else {
+                                $it_point = gc_get_item_point($it);
+                                echo gc_number_format($it_point).'원';
+                            }
+                            ?>
+                        </span>
+                    </li>
+                    <?php } ?>
                     <?php
-                    if($it['it_point_type'] == 2) {
-                        echo '구매금액(추가옵션 제외)의 '.$it['it_point'].'%';
-                    } else {
-                        $it_point = gc_get_item_point($it);
-                        echo gc_number_format($it_point).'원';
+                    $ct_send_cost_label = __('배송비결제', GC_NAME);
+        
+                    if($it['it_sc_type'] == 1)
+                        $sc_method = __('무료배송', GC_NAME);
+                    else {
+                        if($it['it_sc_method'] == 1)
+                            $sc_method = __('수령후 지불', GC_NAME);
+                        else if($it['it_sc_method'] == 2) {
+                            $ct_send_cost_label = '<label for="ct_send_cost">'.__('배송비결제', GC_NAME).'</label>';
+                            $sc_method = '<select name="ct_send_cost" id="ct_send_cost">
+                                              <option value="0">'.__('주문시 결제', GC_NAME).'</option>
+                                              <option value="1">'.__('수령후 지불', GC_NAME).'</option>
+                                          </select>';
+                        }
+                        else
+                            $sc_method = __('주문시 결제', GC_NAME);
                     }
                     ?>
-                </span>
-            </li>
-            <?php } ?>
-            <?php
-            $ct_send_cost_label = __('배송비결제', GC_NAME);
-
-            if($it['it_sc_type'] == 1)
-                $sc_method = __('무료배송', GC_NAME);
-            else {
-                if($it['it_sc_method'] == 1)
-                    $sc_method = __('수령후 지불', GC_NAME);
-                else if($it['it_sc_method'] == 2) {
-                    $ct_send_cost_label = '<label for="ct_send_cost">'.__('배송비결제', GC_NAME).'</label>';
-                    $sc_method = '<select name="ct_send_cost" id="ct_send_cost">
-                                      <option value="0">'.__('주문시 결제', GC_NAME).'</option>
-                                      <option value="1">'.__('수령후 지불', GC_NAME).'</option>
-                                  </select>';
-                }
-                else
-                    $sc_method = __('주문시 결제', GC_NAME);
-            }
-            ?>
-            <li>
-                <span class="ov_row"><?php echo $ct_send_cost_label; ?></span>
-                <span class="ov_description"><?php echo $sc_method; ?></span>
-            </li>
-            <?php if($it['it_buy_min_qty']) { ?>
-            <li>
-                <span class="ov_row"><?php _e('최소구매수량', GC_NAME); ?></span>
-                <span class="ov_description"><?php echo gc_number_format($it['it_buy_min_qty']); ?> 개</span>
-            </li>
-            <?php } ?>
-            <?php if($it['it_buy_max_qty']) { ?>
-            <li>
-                <span class="ov_row"><?php _e('최대구매수량', GC_NAME); ?></span>
-                <span class="ov_description"><?php echo gc_number_format($it['it_buy_max_qty']); ?> 개</span>
-            </li>
-            <?php } ?>
-            </ul>
-
+                    <li>
+                        <span class="ov_row"><?php echo $ct_send_cost_label; ?></span>
+                        <span class="ov_description"><?php echo $sc_method; ?></span>
+                    </li>
+                    <?php if($it['it_buy_min_qty']) { ?>
+                    <li>
+                        <span class="ov_row"><?php _e('최소구매수량', GC_NAME); ?></span>
+                        <span class="ov_description"><?php echo gc_number_format($it['it_buy_min_qty']); ?> 개</span>
+                    </li>
+                    <?php } ?>
+                    <?php if($it['it_buy_max_qty']) { ?>
+                    <li>
+                        <span class="ov_row"><?php _e('최대구매수량', GC_NAME); ?></span>
+                        <span class="ov_description"><?php echo gc_number_format($it['it_buy_max_qty']); ?> 개</span>
+                    </li>
+                    <?php } ?>
+                </ul>
+                <p class="sns_star">
+                    <?php if ($star_score) { ?>
+                    <img src="<?php echo GC_DIR_URL; ?>img/s_star<?php echo $star_score?>.png" alt="" class="sit_star">
+                    <?php } ?>
+                </p>
+            </div>
+            
+            
             <?php
             if($option_item) {
             ?>
             <!-- 선택옵션 시작 { -->
-            <section>
+            <section class="sit_option">
                 <h3><?php _e('선택옵션', GC_NAME); ?></h3>
                 <ul class="sit_ov_box sit_op_box">
                 <?php // 선택옵션
@@ -213,7 +216,7 @@ $gc_ajax_nonce = wp_create_nonce( "item_form" );
             if($supply_item) {
             ?>
             <!-- 추가옵션 시작 { -->
-            <section>
+            <section class="sit_option">
                 <h3><?php _e('추가옵션', GC_NAME); ?></h3>
                 <ul class="sit_ov_box sit_op_box">
                 <?php // 추가옵션
@@ -269,13 +272,14 @@ $gc_ajax_nonce = wp_create_nonce( "item_form" );
 
             <div id="sit_ov_btn">
                 <?php if ($is_orderable) { ?>
-                <input type="submit" onclick="document.pressed=this.value;" value="<?php _e('바로구매', GC_NAME); ?>" id="sit_btn_buy">
-                <input type="submit" onclick="document.pressed=this.value;" value="<?php _e('장바구니', GC_NAME); ?>" id="sit_btn_cart">
+                <input type="submit" onclick="document.pressed=this.value;" value="<?php _e('바로구매', GC_NAME); ?>" id="sit_btn_buy" class="icon_buy_btn">
+                <input type="submit" onclick="document.pressed=this.value;" value="<?php _e('장바구니', GC_NAME); ?>" id="sit_btn_cart" class="icon_cart_btn">
+                
                 <?php } ?>
                 <?php if(!$is_orderable && $it['it_soldout'] && $it['it_stock_sms']) { ?>
-                <button type="button" data-itid="<?php echo $it['it_id']; ?>" title="<?php _e('상품 재입고알림 SMS', GC_NAME); ?>" id="sit_btn_buy"><?php _e('재입고알림', GC_NAME); ?></button>
+                <button type="button" data-itid="<?php echo $it['it_id']; ?>" title="<?php _e('상품 재입고알림 SMS', GC_NAME); ?>" id="sit_btn_buy" class="sms_alram"><i class="fa fa-bell sms_alram_icon" aria-hidden="true"></i> <?php _e('재입고알림', GC_NAME); ?></button>
                 <?php } ?>
-                <button type="button" onclick="gc_item_wish(document.fitem, '<?php echo $it['it_id']; ?>');" id="sit_btn_wish"><?php _e('위시리스트', GC_NAME); ?></button>
+                <button type="button" onclick="gc_item_wish(document.fitem, '<?php echo $it['it_id']; ?>');" id="sit_btn_wish"><i class="fa fa-heart icon_wish_btn" aria-hidden="true"></i> <?php _e('위시리스트', GC_NAME); ?></button>
             </div>
 
             <script>
@@ -324,21 +328,22 @@ $gc_ajax_nonce = wp_create_nonce( "item_form" );
         </li>
     </ul>
     <!-- } 상품 요약정보 및 구매 끝 -->
-
-        <!-- 다른 상품 보기 시작 { -->
-        <div id="sit_siblings">
-            <?php
-            if ($prev_href || $next_href) {
-                echo $prev_href.$prev_title.$prev_href2;
-                echo $next_href.$next_title.$next_href2;
-            } else {
-                echo '<span class="sound_only">'.__('이 분류에 등록된 다른 상품이 없습니다.', GC_NAME).'</span>';
-            }
-            ?>
-        </div>
-        <!-- } 다른 상품 보기 끝 -->
+    <div id="sit_star_sns" >
+        <?php echo $sns_share_links; ?>
+    </div>
+    <!-- 다른 상품 보기 시작 { -->
+    <div id="sit_siblings">
+        <?php
+        if ($prev_href || $next_href) {
+            echo '<span class="prev">' .$prev_href.$prev_title.$prev_href2.'</span>';
+            echo '<span class="next">'.$next_href.$next_title.$next_href2.'</span>';
+        } else {
+            echo '<span class="sound_only">'.__('이 분류에 등록된 다른 상품이 없습니다.', GC_NAME).'</span>';
+        }
+        ?>                    
+    </div>
+    <!-- } 다른 상품 보기 끝 -->
 </div>
-
 </form>
 
 <?php
